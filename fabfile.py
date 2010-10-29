2a72d858ea948f380bd2a8e4e4a7cd894b2e3c75
Kenneth Reitz <me@kennethreitz.com>
2010-10-29 00:30:42 -0400

    Basic project structure in place.

---
+++ fabfile.py
@@ -0,0 +1,8 @@
+import os
+from fabric.api import *
+
+
+def scrub():
+	""" Death to the bytecode! """
+	local('rm -fr dist build')
+	local("find . -name \"*.pyc\" -exec rm '{}' ';'")
---
+++ setup.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+from distutils.core import setup
+
+
+def publish():
+	"""Publish to PyPi"""
+	os.system("python setup.py sdist upload")
+
+if sys.argv[-1] == "publish":
+	publish()
+	sys.exit()
+
+required = []
+
+# if sys.version_info < (2, 6):
+# 	required.append('simplejson')
+
+setup(
+	name='pyandoc',
+	version='0.1.0',
+	description='Python wrapper for Pandoc - the universal document converter',
+	long_description=open('README.rst').read() + '\n\n' +
+	                 open('HISTORY.rst').read(),
+	author='Kenneth Reitz',
+	author_email='me@kennethreitz.com',
+	url='http://tablib.org',
+	packages= [
+		'tablib', 'tablib.formats', 
+		'tablib.packages',
+		'tablib.packages.simplejson',
+		'tablib.packages.xlwt',
+		'tablib.packages.yaml', 
+	],
+	install_requires=required,
+	license='MIT',
+	classifiers=(
+		'Development Status :: 5 - Production/Stable',
+		'Intended Audience :: Developers',
+		'Natural Language :: English',
+		'License :: OSI Approved :: MIT License',
+		'Programming Language :: Python',
+        # 'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+		'Programming Language :: Python :: 2.7',
+		# 'Programming Language :: Python :: 3.0',
+		# 'Programming Language :: Python :: 3.1',
+	),
+    # entry_points={
+    #   'console_scripts': [
+    #       'tabbed = tablib.cli:start',
+    #   ],
+    # }
+)
